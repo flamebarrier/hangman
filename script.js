60a6938c204781930aca921966c1d327e75f8ad2
Benny <flamebarrier@gmail.com>
2022-05-21 09:30:46 +0300

    Hangman first commit.

---
+++ script.js
@@ -0,0 +1,94 @@
+const manParts       = document.querySelectorAll('.figure')
+
+const wrongLettersEl = document.querySelector('#wrong-letters')
+const wordEl         = document.querySelector('#word')
+const endgamePopup   = document.querySelector('#popup-container')
+const finalMessage   = document.querySelector('#final-message')
+const playAgainBtn   = document.querySelector('#play-button')
+const notification   = document.querySelector('#notification-container')
+
+// const wordsArr = ['application', 'programming', 'interface', 'wizard', 'perseverance']
+
+// let selectedWord = wordsArr[Math.floor(Math.random() * wordsArr.length)]
+
+let wrongLettersArr = []
+const correctLettersArr = ['a', 'i', 'e']
+let unknownWord
+
+function showWord() {
+    wordEl.innerHTML = `
+    ${unknownWord
+        .split("")
+        .map(letter => `<span class="letter">${correctLettersArr.includes(letter) ? letter : ""}</span>`)
+        .join("")}
+        `
+        
+        const innerWord = wordEl.innerText.replace(/\n/g, '')
+        
+        if (innerWord === unknownWord) {
+            endgamePopup.style.display = 'flex'
+            finalMessage.innerText = 'Congradulations! You won!'
+        }
+}
+    
+async function getWord() {
+    
+    const response = await fetch('https://random-word-api.herokuapp.com/word');
+    
+    const data = await response.json()
+    
+    const selectedWord = data[0]
+
+    console.log(selectedWord)
+    
+    unknownWord = selectedWord
+    
+    showWord()
+}
+
+function updateWrongLetters() {
+    wrongLettersEl.innerHTML = `
+    ${wrongLettersArr.length >= 1 ? "Wrong Letters" : ""}
+    <span>${wrongLettersArr}</span>
+    `
+
+}
+    
+function restartGame() {
+    endgamePopup.style.display = 'none'
+    wrongLettersArr =[]
+    updateWrongLetters()
+    console.log("restart")
+    getWord()
+}
+
+window.addEventListener('keydown', e => {   
+    if (e.code.includes("Key")) {
+        if (unknownWord.includes(e.key)) {
+            if (correctLettersArr.includes(e.key)) {
+                notification.classList.add("show")
+                setTimeout(() => {
+                    notification.classList.remove("show");
+                }, 2000);
+            } else {
+                correctLettersArr.push(e.key)
+                showWord()
+            }
+        } else {
+            if (!wrongLettersArr.includes(e.key)) {
+                wrongLettersArr.push(e.key)
+                updateWrongLetters()
+            } else {
+                notification.classList.add("show")
+                setTimeout(() => {
+                    notification.classList.remove("show");
+                }, 2000);
+            }
+        }
+    }
+})   
+
+playAgainBtn.addEventListener('click', restartGame)
+  
+getWord()
+
